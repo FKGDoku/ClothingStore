40f28f8bf1702ea306aa24895ec2a139fb9ccc16
FKGDoku <lazarevdaniil105@gmail.com>
2025-10-24 18:48:40 +0300

    registration and animation
    new animations

--- resources/views/MainPage.blade.php
+++ resources/views/MainPage.blade.php
@@ -10,12 +10,29 @@
 </head>
 <body class="bg-black">
 
-<footer class="cursor-pointer flex justify-center items-center gap-25 text-white/80 font-light">
-    <h1 class="duration-200 ease-in-out active:text-white text-xl">футболки</h1>
-    <h1 class="duration-200 ease-in-out active:text-white text-xl text-xl">свитшоты</h1>
-    <h1 class="duration-200 ease-in-out active:text-white text-xl text-xl">верхняя одежда</h1>
-    <h1 class="duration-200 ease-in-out active:text-white text-xl text-xl">аксессуары</h1>
-    <h1 class="duration-200 ease-in-out active:text-white text-xl text-xl">все товары</h1>
+<footer class=" cursor-pointer text-white/80 font-light">
+    <div class="ml-2 mt-2 mb-2 flex gap-5">
+    <h1 class="rounded active:bg-white/70 active:shadow-lg shadow-white/50 duration-200 ease-in-out active:text-black"> регистрируйся </h1>
+    <h1> / </h1>
+    <h1 class="rounded active:bg-white/70 active:shadow-lg shadow-white/50 duration-200 ease-in-out active:text-black"> входи </h1>
+
+        <input class="border-white/80 border border-rounded rounded-lg " type="text" placeholder="Поиск">
+
+        <h1 class="active:shadow-lg shadow-white/40 rounded-lg duration-300 ease-in-out active:text-black active:bg-yellow-700 mr-5 ml-auto">быть Русским - жить вечно</h1>
+    </div>
+
+    <div class="flex justify-center items-center gap-25">
+        <h1 class="rounded-lg active:bg-white/70 active:shadow-lg shadow-white/50 duration-200 ease-in-out active:text-black text-xl">
+            футболки</h1>
+        <h1 class="rounded-lg active:bg-white/70 active:shadow-lg shadow-white/50 duration-200 ease-in-out active:text-black text-xl text-xl">
+            свитшоты</h1>
+        <h1 class="rounded-lg active:bg-white/70 active:shadow-lg shadow-white/50 duration-200 ease-in-out active:text-black text-xl text-xl">
+            верхняя одежда</h1>
+        <h1 class="rounded-lg active:bg-white/70 active:shadow-lg shadow-white/50 duration-200 ease-in-out active:text-black text-xl text-xl">
+            аксессуары</h1>
+        <h1 class="rounded-lg active:bg-white/70 active:shadow-lg shadow-white/50 duration-200 ease-in-out active:text-black text-xl text-xl">
+            все товары</h1>
+    </div>
 </footer>
 
 <div class="mt-5 flex justify-center items-center">
@@ -23,7 +40,7 @@
 
         <summary class="font-light text-white/80 flex items-center gap-2 list-none cursor-pointer">
             цвета
-            <svg class="caret w-4 h-4 transition-transform duration-200"
+            <svg class="caret w-4 h-4 transition-transform duration-300"
                  viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd"
                       d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.06 1.06l-4.24 4.25a.75.75 0 01-1.06 0L5.21 8.29a.75.75 0 01.02-1.08z"
@@ -34,7 +51,7 @@
 
         <div class="font-light text-white/80 mt-2 rounded-md p-3 shadow-md z-10 min-w-[180px]">
             <ul class="cursor-pointer flex-col flex gap-2 justify-center">
-                <!-- цвет: просто кнопка/свитч -->
+
                 <li class="active:border-white border border-1 border-white/50 active:text-white duration-150 ease-in-out">
                     белый
                 </li>
